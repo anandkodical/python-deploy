0df5c5d40e14adabcb202b0ed8fe8e59c491dd66
Ananda K <110787791+anandkodical@users.noreply.github.com>
2025-04-14 17:25:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, This is Anand from Mangalore...."
+    return "Hello, This is Anand from Mangalore.."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
